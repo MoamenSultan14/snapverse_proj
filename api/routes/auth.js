4305b1a3ca6cb19248187d22f11e03a67e583ad7
MoamenSultan14 <shawesh9814@gmail.com>
2024-06-14 14:08:24 +0300

    test1

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+
 
 //register
 router.post("/register", async (req,res) => {
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const ObjectId = require('mongoose').Types.ObjectId;
 
 
