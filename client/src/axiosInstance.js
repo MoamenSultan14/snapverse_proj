8475db5e61cfa3f94d7cd06ecb5c74e6201a7ce9
MoamenSultan14 <shawesh9814@gmail.com>
2024-06-17 16:31:37 +0300

    baseUrl

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://snapverse-proj-api.onrender.com', // Replace with your server's base URL
+  baseURL: 'https://snapverse-proj-api.onrender.com/api', // Replace with your server's base URL
 //   timeout: 10000, // Optional: Set a timeout for requests
 //   headers: {
 //     'Content-Type': 'application/json',
