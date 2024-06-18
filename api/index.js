ea69d8c83417b1da68e82377931c080da47fca69
MoamenSultan14 <shawesh9814@gmail.com>
2024-06-18 13:27:42 +0300

    corsOptions

--- api/index.js
+++ api/index.js
@@ -169,7 +169,7 @@ const corsOptions = {
     allowedHeaders: ['Content-Type', 'Authorization'], 
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
